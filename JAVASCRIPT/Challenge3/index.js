dacde83efb7ef5d006025c6e355e0eda84124b74
devRonPark <pbc9236@gmail.com>
2024-01-22 16:08:22 +0900

    JavaScript 챌린지 3 과제 수정:변수 선언

--- JAVASCRIPT/Challenge3/index.js
+++ JAVASCRIPT/Challenge3/index.js
@@ -1,11 +1,11 @@
 function printUserInfo() {
-  myName = prompt("당신의 이름은?", "홍길동");
-  myHobby = prompt("당신의 취미는?", "독서");
-  favSong = prompt("좋아하는 곡은?", "어른");
+  const myName = prompt("당신의 이름은?", "홍길동");
+  const myHobby = prompt("당신의 취미는?", "독서");
+  const favSong = prompt("좋아하는 곡은?", "어른");
 
-  myNameElem = document.getElementById("name");
-  myHobbyElem = document.getElementById("hobby");
-  favSongElem = document.getElementById("song");
+  const myNameElem = document.getElementById("name");
+  const myHobbyElem = document.getElementById("hobby");
+  const favSongElem = document.getElementById("song");
 
   myNameElem.innerText = myName;
   myHobbyElem.innerText = myHobby;
